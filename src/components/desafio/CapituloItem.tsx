1120ae5462dd094c6617495612c0b9a03e1a935e
cristianoramon <cristianramon@yahoo.com.br>
2024-11-11 13:28:27 -0300

    Aula express

--- src/components/desafio/CapituloItem.tsx
+++ src/components/desafio/CapituloItem.tsx
@@ -9,16 +9,17 @@ interface CapituloItemProps {
 
 export default function CapituloItem(props: CapituloItemProps) {
     const { capitulo } = props
-    const { aulaAtual } = useContext(CursoContext)
-    return (
-        <div>
+    const { aulaAtual,selecionarAulasCapituloConcluidas,selecionaStatusAulas } = useContext(CursoContext)
+
+      return (
+        <div className='text-zinc-300'>
             <div className="flex items-center gap-3 p-4 bg-zinc-900">
                 <div
-                    className="
-                    flex justify-center items-center
-                    h-9 w-9 rounded-full bg-black
-                    border border-zinc-400
-                "
+                    className={`
+                     flex justify-center items-center
+                      h-9 w-9 rounded-full ${selecionaStatusAulas ? 'bg-green-500':'bg-black'}
+                      border border-zinc-400`}
+                onClick={() => selecionarAulasCapituloConcluidas(capitulo)}
                 >
                     {capitulo.ordem}
                 </div>
--- src/context/CursoContext.tsx
+++ src/context/CursoContext.tsx
@@ -1,5 +1,5 @@
 import Capitulo from '@/data/model/Capitulo'
-import { createContext, useState } from 'react'
+import { createContext, useReducer, useRef, useState } from 'react'
 import capitulosFake from '@/data/constants/capitulos'
 import Aula from '@/data/model/Aula'
 import CursoEstatisticas from '@/data/model/CursoEstatisticas'
@@ -12,8 +12,11 @@ interface CursoContextProps {
     duracaoTotal: string
     duracaoConcluida: string
     percentualConclusao: number
+    selecionaStatusAulas:boolean
     selecionarAula: (aula: Aula) => void
     alternarConclusaoAula: (aula: Aula) => void
+    selecionarAulasCapituloConcluidas: (capitulo: Capitulo) => void
+
 }
 
 const CursoContext = createContext<CursoContextProps>({} as any)
@@ -21,8 +24,8 @@ const CursoContext = createContext<CursoContextProps>({} as any)
 export function CursoProvider(props: any) {
     const [capitulos, setCapitulos] = useState(capitulosFake)
     const [aulaAtual, setAulaAtual] = useState(capitulosFake[0].aulas[0])
-
-    const cursoEstatisticas = new CursoEstatisticas(capitulos)
+    const boolTodasAulas            = useRef(false);
+    const cursoEstatisticas         = new CursoEstatisticas(capitulos)
 
     function alternarConclusaoAula(aulaSelecionada: Aula) {
         const novosCapitulos = capitulos.map((capitulo) => {
@@ -39,6 +42,26 @@ export function CursoProvider(props: any) {
     function selecionarAula(aula: Aula) {
         setAulaAtual(aula)
     }
+    function selecionaTodasAulas() {
+        boolTodasAulas.current = !boolTodasAulas.current;
+        return boolTodasAulas.current;
+    };
+
+    function selecionarAulasCapituloConcluidas(capituloSelecionado: Capitulo) {
+
+        const boolSelecao = selecionaTodasAulas();
+
+        const aulasConcluidas = capitulos.map( (cap) =>{
+            if ( cap.id === capituloSelecionado.id ) {
+                return {...cap, aulas: cap.aulas.map( (aula) => {
+                    return {...aula, concluida:boolSelecao}
+                })}
+            } else {
+                return {...cap}
+            }
+        });
+        setCapitulos(aulasConcluidas);
+    }
 
     return (
         <CursoContext.Provider
@@ -47,6 +70,8 @@ export function CursoProvider(props: any) {
                 aulaAtual,
                 selecionarAula,
                 alternarConclusaoAula,
+                selecionarAulasCapituloConcluidas,
+                get selecionaStatusAulas() { return boolTodasAulas.current },
                 get qtdeDeAulas() { return cursoEstatisticas.qtdeDeAulas() },
                 get aulasConcluidas() { return cursoEstatisticas.aulasConcluidas() },
                 get duracaoTotal() { return cursoEstatisticas.duracaoTotal() },
